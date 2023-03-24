d0d9f1fc61829dbd9051a04ab7ff7c79901bda06
Trycle00 <trycle00@gmail.com>
2023-03-24 10:50:57 +0800

    Update openvpn-install.sh
    Define output directory of the vpn config

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -4,6 +4,9 @@
 #
 # Copyright (c) 2013 Nyr. Released under the MIT License.
 
+# Define output directory of the vpn config
+CONFIG_DIR=~/.ovpn/vconfig
+mkdir -p ${CONFIG_DIR}
 
 # Detect Debian users running the script with "sh" instead of bash
 if readlink /proc/$$/exe | grep -q "dash"; then
@@ -95,7 +98,7 @@ new_client () {
 	echo "<tls-crypt>"
 	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
 	echo "</tls-crypt>"
-	} > ~/"$client".ovpn
+	} > ${CONFIG_DIR}/"$client".ovpn
 }
 
 if [[ ! -e /etc/openvpn/server/server.conf ]]; then
@@ -433,7 +436,7 @@ verb 3" > /etc/openvpn/server/client-common.txt
 	echo
 	echo "Finished!"
 	echo
-	echo "The client configuration is available in:" ~/"$client.ovpn"
+	echo "The client configuration is available in:" ${CONFIG_DIR}/"$client.ovpn"
 	echo "New clients can be added by running this script again."
 else
 	clear
@@ -465,7 +468,7 @@ else
 			# Generates the custom client.ovpn
 			new_client
 			echo
-			echo "$client added. Configuration available in:" ~/"$client.ovpn"
+			echo "$client added. Configuration available in:" ${CONFIG_DIR}/"$client.ovpn"
 			exit
 		;;
 		2)
